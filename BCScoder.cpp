831bd78801bc7e48e5acec123b828c9f8442fef6
k4kaylee <roman.kulpekshark@gmail.com>
2023-07-24 12:15:49 +0200

    Restructurisation. Adding string encoding and decoding, rebuilding existing uint coding functions

--- BCScoder.cpp
+++ BCScoder.cpp
@@ -7,60 +7,99 @@
 #include <cassert>
 
 
+#include <vector>
+#include <cstdint>
+#include <sstream>
+#include <iostream>
+#include <cassert>
+#include "../Private/BCScoderModule.h"
+
+
+
+
+
+void FBCSencoderModule::encode(BCS_value& value) {
+    switch (value.type) {
+    case BCS_value::BCS_NUMBER: encodeUnsignedInt(value.data.number); break;
+    case BCS_value::BCS_STRING: encodeUTF8String(value.data.string); break;
+
+    }
+}
+
+//Encoders
+
 template <typename T>
-std::vector<uint8_t> FBCSencoderModule::encodeUnsignedInt(T value) {
-    std::vector<uint8_t> bytes;
+void FBCSencoderModule::encodeUnsignedInt(T value) {
     size_t size = sizeof(T);
     for (size_t i = 0; i < size; ++i) {
-        bytes.push_back(static_cast<uint8_t>(value >> (i * 8)));
+        char byte = static_cast<char>(value >> (i * 8));
+        _output.write(&byte, 1);
     }
-    return bytes;
+}
+
+void FBCSencoderModule::encodeUTF8String(const std::string& utf8String) {
+    _output.write(utf8String.data(), utf8String.size());
 }
 
 
+
+// Decoders
+
 template <typename T>
-T FBCSencoderModule::decodeUnsignedInt(const std::vector<uint8_t>& bytes) {
-    if (bytes.size() != sizeof(T)) {
-        throw std::runtime_error("Incorrect size for decoding unsigned integer.");
-    }
+T FBCSdecoderModule::decodeUnsignedInt() {
+    char bytes[sizeof(T)];
+    _input.read(bytes, sizeof(T));
     T value = 0;
-    for (size_t i = 0; i < bytes.size(); ++i) {
+    for (size_t i = 0; i < sizeof(T); ++i) {
         value |= static_cast<T>(bytes[i]) << (i * 8);
     }
     return value;
 }
 
-
-void runAllTests() {
-    FBCSencoderModule encoder;
-
-    //u8
-    uint8_t u8Value = 42;
-    std::vector<uint8_t> u8Bytes = encoder.encodeUnsignedInt(u8Value);
-    uint8_t decodedU8Value = encoder.decodeUnsignedInt<uint8_t>(u8Bytes);
-    assert(u8Value == decodedU8Value);
-
-    //u16
-    uint16_t u16Value = 12345;
-    std::vector<uint8_t> u16Bytes = encoder.encodeUnsignedInt(u16Value);
-    uint16_t decodedU16Value = encoder.decodeUnsignedInt<uint16_t>(u16Bytes);
-    assert(u16Value == decodedU16Value);
-
-    //u32
-    uint32_t u32Value = 1234567890;
-    std::vector<uint8_t> u32Bytes = encoder.encodeUnsignedInt(u32Value);
-    uint32_t decodedU32Value = encoder.decodeUnsignedInt<uint32_t>(u32Bytes);
-    assert(u32Value == decodedU32Value);
-
-    //u64
-    uint64_t u64Value = 12345678901234567890ULL;
-    std::vector<uint8_t> u64Bytes = encoder.encodeUnsignedInt(u64Value);
-    uint64_t decodedU64Value = encoder.decodeUnsignedInt<uint64_t>(u64Bytes);
-    assert(u64Value == decodedU64Value);
+std::string FBCSdecoderModule::decode_string() {
+    size_t len = read_uleb128();
+    std::string str(len, '\0');
+    _input.read(const_cast<char*>(str.data()), len);
+    return str;
 }
 
+template <typename T, typename F>
+std::vector<T> FBCSdecoderModule::decodeArray(F reader) {
+    size_t len = read_uleb128();
+    std::vector<T> data(len);
+    for (size_t i = 0; i < len; i++) {
+        data[i] = reader(this);
+    }
+    return data;
+}
+
+
+
+// Misc
+
+uint64_t FBCSdecoderModule::read_uleb128() {
+    uint64_t result = 0;
+    int shift = 0;
+    uint8_t byte;
+    do {
+        _input.read(reinterpret_cast<char*>(&byte), 1);
+        uint64_t bits = byte & MASK(7);
+        result |= bits << shift;
+        shift += 7;
+    } while ((byte & BIT(7)) == 0);
+    return result;
+}
+
+
+// Unit tests
+void runAllTests() {
+    /* Under reconstruction */
+}
+
+
+
 int main() {
-    runAllTests();
+    //runAllTests();
 
     return 0;
 }
